bbcd1e7779744c023bb0a9cef39700f50f2ac677
dachacond <102201222+dachacond@users.noreply.github.com>
2025-04-25 18:12:58 -0500

    feat: Add Role model with choices and link to User

--- app/models.py
+++ app/models.py
@@ -4,10 +4,35 @@ from django.db import models
 from app.storage import PDFStorage
 
 
-class User(AbstractUser):
-    created_at = models.DateTimeField(auto_now_add=True)
+
+class RoleChoices(models.TextChoices):
+    EMPLEADO = 'empleado', 'Empleado'
+    CLIENTE = 'cliente', 'Cliente'
+
+
+
+class Role(models.Model):
+    name = models.CharField(
+        max_length=10,
+        choices=RoleChoices.choices,
+        unique=True 
+    )
 
     def __str__(self):
+        return self.get_name_display() 
+
+
+class User(AbstractUser):
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    roles = models.ManyToManyField(
+        Role,
+        blank=True, 
+        related_name="users"
+    )
+
+    def __str__(self):
+        
         return f"{self.first_name} {self.last_name} ({self.email})"
 
 
