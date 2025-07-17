253921c836f7d26a057e8538726618c298bc9fdf
Juan A. Rodríguez <84048520+jrodriguezru@users.noreply.github.com>
2025-07-17 08:19:19 -0500

    feat/Fix Migrations Inconsistency 2

--- app/migrations/0024_processchecklistitem_due_date.py
+++ app/migrations/0024_processchecklistitem_due_date.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("app", "0021_processchecklistitem_status_checklistitemstatuslog"),
+        ("app", "0023_alter_report_estado_reporte"),
     ]
 
     operations = [
